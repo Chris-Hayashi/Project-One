3feb003763684e52cebc4059c179106261bf96d3
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-11 19:32:34 -0800

    created working files

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCub0rY9jJfPV7_M0hR8HDby2zg34od4oE",
+    authDomain: "spectator-b36b8.firebaseapp.com",
+    databaseURL: "https://spectator-b36b8.firebaseio.com",
+    projectId: "spectator-b36b8",
+    storageBucket: "spectator-b36b8.appspot.com",
+    messagingSenderId: "452594229506",
+    appId: "1:452594229506:web:a1d9ed18a9eaf03208621b",
+    measurementId: "G-3ZRD15YJRY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  
