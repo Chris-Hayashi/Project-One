ff4a613336a23e08e3543cd9de1417640fe2597e
Brandon Shankar <bshankar12@comcast.net>
2019-11-13 21:49:01 -0800

    stuff

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,8 @@
 //popper on movie card
-    var reference = document.querySelector('.movie-card');
-    var popper = document.querySelector('.movie-popper');
-    var popperInstance = new Popper(reference, popper, {
-        
-    });
-    var popper = new Popper(referenceElement, onPopper, {
-        placement: 'right'
-    });
+    var reference = $(".movie-card");
+    var popup = $(".movie-popper");
+    popup.hide();
+
+    reference.click(function(){    
+        popup.show();
+    })
